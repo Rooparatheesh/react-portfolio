5c9fb0a3ba57f19d4f3a73a50326229cf50d9344
Rooparatheesh <73030282+Rooparatheesh@users.noreply.github.com>
2025-05-06 15:36:14 +0530

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Roopa Ratheesh</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 6 months of experience in React and Node.js, along with hands-on expertise in Flutter. Feel free to reach out to learn more! 🚀
+        </p>
+        <a href="mailto:rooparatheesh2000@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of Roopa Ratheesh"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
